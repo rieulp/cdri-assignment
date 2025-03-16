9d2775f4167a66c4d2f8c3108c848d7f126e4389
rieulp <rayoung1229@gmail.com>
2025-03-16 22:33:08 +0900

    fix: 헤더 배경색 및 텍스트 색상 수정

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,21 +3,17 @@ import Link from 'next/link';
 
 export function Header() {
   return (
-    <header className="w-full px-8 bg-p-white">
+    <header className="w-full px-8 bg-white">
       <div className="h-32 max-w-[1600px] w-full mx-auto grid grid-cols-[20.7rem_minmax(max-content,1fr)_minmax(0,20.7rem)] items-center space-x-[5.6rem]">
         {/* 로고 */}
         <Link href="/">
-          <div className="text-title1 text-t-primary select-none">CERTICOS BOOKS</div>
+          <div className="text-title1 text-text-primary select-none">CERTICOS BOOKS</div>
         </Link>
-        <nav className="flex items-center flex-1 justify-center gap-[5.6rem] text-t-primary text-body1">
-          <ActiveLink href="/" className="h-[3.2rem] pt-[0.3rem]" activeClassName="border-b-p-primary border-b">
+        <nav className="flex items-center flex-1 justify-center gap-[5.6rem] text-text-primary text-body1">
+          <ActiveLink href="/" className="h-[3.2rem] pt-[0.3rem]" activeClassName="border-b-primary border-b">
             도서 검색
           </ActiveLink>
-          <ActiveLink
-            href="/favorites"
-            className="h-[3.2rem] pt-[0.3rem]"
-            activeClassName="border-b-p-primary border-b"
-          >
+          <ActiveLink href="/favorites" className="h-[3.2rem] pt-[0.3rem]" activeClassName="border-b-primary border-b">
             내가 찜한 책
           </ActiveLink>
         </nav>
