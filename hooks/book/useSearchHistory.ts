840fc0b4864a2ac7c3b807be1b38e8f31cbe2c73
rieulp <rayoung1229@gmail.com>
2025-03-18 12:56:22 +0900

    feat: 검색 기록의 최대 개수를 8로 제한

--- hooks/book/useSearchHistory.ts
+++ hooks/book/useSearchHistory.ts
@@ -1,9 +1,11 @@
 import { useLocalStorage } from '../useLocalStorage';
 
+const LIMIT = 8;
+
 export const useSearchHistory = () => {
   const [searchHistory, setSearchHistory] = useLocalStorage<string[]>('searchHistory', []);
   const add = (search: string) => {
-    const newSearchHistory = Array.from(new Set([search, ...searchHistory]));
+    const newSearchHistory = Array.from(new Set([search, ...searchHistory])).slice(0, LIMIT);
     setSearchHistory(newSearchHistory);
   };
   const remove = (index: number) => {
