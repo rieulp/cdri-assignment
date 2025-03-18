1b660511a365ac9b3e2289377fd238252b350acc
rieulp <rayoung1229@gmail.com>
2025-03-18 11:59:59 +0900

    feat: 홈 페이지에 도서 검색 기능 추가
    - SearchBooksSection을 추가하여 도서 검색 기능 구현
    - React Query를 활용한 prefetchInfiniteQuery 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
-export default function Home() {
+import React from 'react';
+import DefaultLayout from '@/components/layout/defaultLayout';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import SearchBooksSection from '@/components/searchBooksSection';
+import { getBooks, SearchBooksQuery } from '@/hooks/book/useSearchBooks';
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    [key: string]: string | string[] | undefined;
+  }>;
+}) {
+  const params = (await searchParams) as unknown as SearchBooksQuery;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ['books', params],
+    queryFn: ({ pageParam = 1 }) => getBooks({ ...params, page: pageParam }),
+    getNextPageParam: (
+      lastPage: Awaited<ReturnType<typeof getBooks>>,
+      allPages: Awaited<ReturnType<typeof getBooks>>[],
+    ) => {
+      return lastPage?.meta.is_end ? undefined : allPages?.length + 1;
+    },
+    initialPageParam: 1,
+  });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="row-start-1 flex gap-[24px] items-center justify-center">header</header>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">main</main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">footer</footer>
-    </div>
+    <DefaultLayout>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <SearchBooksSection />
+      </HydrationBoundary>
+    </DefaultLayout>
   );
 }
