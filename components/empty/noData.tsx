9bda01f69844dd90cce38fcbe4575a4f34275ca5
rieulp <rayoung1229@gmail.com>
2025-03-18 11:46:34 +0900

    feat: NoData 컴포넌트에 className prop 추가 및 스타일링 개선

--- components/empty/noData.tsx
+++ components/empty/noData.tsx
@@ -1,13 +1,15 @@
 import Icon, { IconName } from '@/components/icon';
+import { tw } from '@/utils';
 
 type Props = {
   icon: IconName;
   label: string;
+  className?: string;
 };
 
-export default function NoData({ icon, label }: Props) {
+export default function NoData({ icon, label, className }: Props) {
   return (
-    <div className="flex flex-col items-center gap-[2.4rem]">
+    <div className={tw('flex flex-col items-center gap-[2.4rem]', className)}>
       <Icon name={icon} size="8rem" />
       <p className="text-caption text-text-secondary">{label}</p>
     </div>
